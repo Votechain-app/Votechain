5c51679ba70e417d550e9c607199b1ec36229fda
kris s <kristifer.szabo@gmail.com>
2018-05-14 14:58:27 +0000

    added parts

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+  <head>
+    <title>Votechain</title>
+    <!--Bootstrap4 CSS-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!--Custom CSS loaded after bs-->
+    <link rel="stylesheet" href="styles.css">
+  </head>
+  <body>
+
+    <!-- Using PHP include in order to keep project structure manageable -->
+    <?php 
+    include 'parts/menu.php'; 
+    include 'parts/body.php'; 
+    include 'parts/footer.php'; 
+    ?>
+    
+  </body>
+</html>   
